e9a2dc22eb614afbfc9f2b6c9f9f72c97bd25ca8
eddypaz <eddypaz150893@gmail.com>
2019-12-01 22:02:27 -0600

    subiendo cambios casi completo

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
-
+//Requerir hbs
+const hbs = require('hbs');
+//Esto es un middleware
 app.use(express.static(__dirname + '/public'));
+//Epress HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
+hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitalizar', (texto) => {
+    //Separamos la cadena y la convertimos a arreglo
+    //recibe como parametro el caracter de separacion en este caso un espacio
+    let palabras = texto.split(' ');
+    //Recorremos el arreglo con dos argumentos
+    //1. el arreglo
+    //2. el nombre de la variable para el indice
+    palabras.forEach((palabra, idx) => {
+        //Con slice tomamos de la palabra solo los caracteres de la posicion 1 en adelante
+        palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    });
+    //Juntar todo el arreglo pero separandolo por un espacio recibido como parametro.
+    return palabras.join(' ')
+});
+app.get('/salida', (req, res) => {
     // res.send('hello world');
     let salida = {
         nombre: 'Eddy',
@@ -13,7 +36,15 @@ app.get('/', (req, res) => {
     res.send(salida);
 
 });
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'eddy paz'
+    });
+});
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 app.get('/data', (req, res) => {
     res.send('hello world');
 });
